95145af93b4667494905bda9ab4885af120ee788
6531503026 <6531503026@lamduan.mfu.ac.th>
2023-09-04 15:54:54 +0700

    6531503026

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -1,56 +1,55 @@
 package th.mfu;
 
 import java.io.IOException;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//TODO: add webservlet to "/calbmi"
 @WebServlet("/calbmi")
 public class BMICalculatorServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
-        double weight = Double.parseDouble(request.getParameter("weight"));
-        double height = Double.parseDouble(request.getParameter("height"));
-        //TODO: calculate bmi
-        double bmi = calculateBMI(weight, height);
+        String weightStr = request.getParameter("weight");
+        String heightStr = request.getParameter("height");
 
-        //TODO: determine the built from BMI
-        String build = determineBuild(bmi);
-      
-        //TODO: add bmi and built to the request's attribute
-        request.setAttribute("bmi", bmi);
-        request.setAttribute("build", build);
+        if (weightStr != null && !weightStr.isEmpty() && heightStr != null && !heightStr.isEmpty()) {
+            try {
+                double weight = Double.parseDouble(weightStr);
+                double height = Double.parseDouble(heightStr);
 
-        //TODO: forward to jsp
-        request.getRequestDispatcher("/bmiResult.jsp").forward(request, response);
-    }
-        // Calculate BMI
-        private double calculateBMI(double weight, double height) {
-        // BMI formula: weight (kg) / (height (m) * height (m))
-        return weight / (height * height);
-    }
+                double bmi = calculateBMI(weight, height);
+                String build = determineBuild(bmi);
 
-        // Determine build from BMI
-         private String determineBuild(double bmi) {
-        if (bmi < 18.5) {
-            return "Underweight";
-        } else if (bmi < 25) {
-            return "Normal";
-        } else if (bmi < 30) {
-            return "Overweight";
-        } else if (bmi < 35) {
-            return "Obese";
+                request.setAttribute("bmi", Math.round(bmi));
+                request.setAttribute("build", build);
+
+                request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
+            } catch (NumberFormatException e) {
+                response.getWriter().println("Invalid weight or height values.");
+            }
         } else {
-            return "Extremely Obese";
+            response.getWriter().println("Please provide weight and height parameters.");
+        }
+    }
+
+    private double calculateBMI(double weight, double height) {
+        return weight / ((height)*(height));
+    }
+
+    private String determineBuild(double bmi) {
+        if (bmi < 18.5) {
+            return "underweight";
+        } else if (bmi < 25) {
+            return "normal weight";
+        } else if (bmi < 30) {
+            return "overweight";
+        } else if (bmi < 35) {
+            return "obese";
+        } else {
+            return "extremely obese";
         }
     }
 }
-
-
-
--- src/main/webapp/bmi_result.jsp
+++ src/main/webapp/bmi_result.jsp
@@ -1,13 +1,12 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <html>
    <head>
       <title>BMI Calculator</title>
    </head>
-<body>
-<img src="https://www.cdc.gov/healthyweight/images/assessing/bmi-adult-fb-600x315.jpg?_=07167">
-<!--TODO: show bmi below -->
-<h2>Result is <%= %></h2>
-<!--TODO: show built type below -->
-<h3>According to your BMI, you are considered <%= %></h3>
-</body>
+   <body>
+      <img src="https://www.cdc.gov/healthyweight/images/assessing/bmi-adult-fb-600x315.jpg?_=07167">
+      <!-- Display the calculated BMI below -->
+      <h2>Result is <%= request.getAttribute("bmi") %></h2>
+      <!-- Display the determined built type below -->
+      <h3>According to your BMI, you are considered <%= request.getAttribute("build") %></h3>
+   </body>
 </html>
